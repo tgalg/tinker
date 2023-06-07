063674790d0fac08f106071a223f52280e711fb0
tgalg <thomasxgallagher@gmail.com>
2023-06-07 16:16:20 -0500

    chore: prettier formatting

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,9 +5,9 @@
 module.exports = {
   // !STARTERCONF Change the siteUrl
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: "https://www.tgalg.com/",
+  siteUrl: 'https://www.tgalg.com/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
+    policies: [{ userAgent: '*', allow: '/' }],
   },
-};
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app'
 
-import "@/styles/globals.css";
+import '@/styles/globals.css'
 
 /**
  * !STARTERCONF info
@@ -8,7 +8,7 @@ import "@/styles/globals.css";
  */
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
