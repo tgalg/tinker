ba449a0393163c87f719962e828221d1036b06f8
tgalg <thomasxgallagher@gmail.com>
2023-06-07 15:55:23 -0500

    chore: tweaks3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Layout from '@/components/layout/Layout';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import Seo from '@/components/Seo';
+import Layout from "@/components/layout/Layout";
+import ButtonLink from "@/components/links/ButtonLink";
+import UnderlineLink from "@/components/links/UnderlineLink";
+import Seo from "@/components/Seo";
 
 /**
  * SVGR Support
@@ -12,11 +12,6 @@ import Seo from '@/components/Seo';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Vercel from '~/svg/Vercel.svg';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
 
 export default function HomePage() {
   return (
@@ -25,17 +20,15 @@ export default function HomePage() {
       <Seo />
 
       <main>
-        <section className='bg-white'>
-          <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-            <Vercel className='text-5xl' />
-
-            <ButtonLink className='mt-6' href='/components' variant='light'>
+        <section className="bg-white">
+          <div className="layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center">
+            <ButtonLink className="mt-6" href="/components" variant="light">
               See all components
             </ButtonLink>
 
-            <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://www.tgalg.com/'>
+            <footer className="absolute bottom-2 text-gray-700">
+              © {new Date().getFullYear()} By{" "}
+              <UnderlineLink href="https://www.tgalg.com/">
                 Tom Gallagher
               </UnderlineLink>
             </footer>
